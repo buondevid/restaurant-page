c29cd52c879ed7e8f6999f2a74e1fa6646cd711a
buondevid <davidebriano@gmail.com>
2021-02-22 00:45:14 +0100

    finished menu section, polish style, add js logic

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 import skeletonLoad from './mod_page-load';
+import homeLoad from './mod_home';
+import menuLoad from './mod_menu';
 
-//skeletonLoad();
+skeletonLoad(); // render title,nav and signature
+homeLoad(); // render home
+
+function clearButtonClasses() {
+	const buttons = document.querySelectorAll('button');
+	buttons.forEach((btn) => btn.classList.remove('activeButton'));
+}
+
+function setActiveButton(target) {
+	target.classList.add('activeButton');
+}
+
+(function switchTabs() {
+	const nav = document.querySelector('nav');
+	nav.addEventListener('click', (e) => {
+		if (e.target.nodeName === 'BUTTON') {
+			clearButtonClasses();
+			switch (e.target.innerHTML) {
+			case 'Home':
+				setActiveButton(e.target);
+				homeLoad();
+				break;
+			case 'Menu':
+				setActiveButton(e.target);
+				menuLoad();
+				break;
+			// case 'Contact':
+			// 	contactLoad();
+			// 	break;
+			default:
+				console.log('nothing here');
+			}
+		}
+	});
+}());
+// menuLoad(); // render menu
---
+++ src/mod_menu.js
@@ -0,0 +1,41 @@
+export default function menuLoad() {
+	const main = document.querySelector('main');
+
+	main.innerHTML = `
+				<section class='menu'>
+				<ul>
+					<h2>Starters</h2>
+					<li>
+						<h3>Carpaccio di manzo</h3>
+						<p>Carpaccio crudo con scaglie di grana e olio EVO</p>
+					</li>
+					<li>
+						<h3>Tagliere di salumi e formaggi</h3>
+						<p>Ricco tagliere dai nostri contadini locali</p>
+					</li>
+					<h2>Main courses</h2>
+					<li>
+						<h3>Penne all'arrabbiata</h3>
+						<p>Pasta corta con pomodoro biologico e peperoncino calabrese</p>
+					</li>
+					<li>
+						<h3>Lasagne della nonna</h3>
+						<p>Delicata sfoglia con ragù fatto in casa e besciamella gourmet</p>
+					</li>
+					<li>
+						<h3>Grigliata di carne</h3>
+						<p>Mix di carni provenienti dai migliori tagli di macelleria</p>
+					</li>
+					<h2>Desserts</h2>
+					<li>
+						<h3>Tiramisù</h3>
+						<p>Un evergreen secondo ricetta tramandata da 4 generazioni</p>
+					</li>
+					<li>
+						<h3>Torta A Casa</h3>
+						<p>Il nostro simbolo: una crostata alla frutta con mousse pasticcera</p>
+					</li>
+				</ul>
+			</section>
+			`;
+}
