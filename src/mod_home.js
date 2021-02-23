685a228e8dce683e95041fb8a7fc8d6fd6dc69e6
buondevid <davidebriano@gmail.com>
2021-02-23 11:56:08 +0100

    add SASS to project

--- src/mod_home.js
+++ src/mod_home.js
@@ -6,7 +6,7 @@ export default function homeLoad() {
 	<p><span>A Casa</span> it's a cozy family-owned restaurant, produly in business since 1958.</p>
 	<p>Our main focus is to make you feel home, and that's where our name comes from: in <span>Italian</span>, A Casa means At Home.</p>
 	<p>You will discover a truly innovative taste combined with an authentic <span>traditional</span> expertise... <span>Buon Appetito!</span></p>
-	<img src="/dist/images/philli-tweetie-pzdePziknBQ-unsplash.jpg" alt='old woman chef' id='old-woman-chef'>
+	<img src="images/philli-tweetie-pzdePziknBQ-unsplash.jpg" alt='old woman chef' id='old-woman-chef'>
 </section>
 `;
 }
